40294606c759551db3c2c34c47ec573d90a427b0
Bassel-11 <albassel.abobakr11@gmail.com>
2024-11-18 16:28:46 +0200

    Adding data to the Category Table

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -9,5 +9,14 @@ namespace BulkyWeb.Data
         {
         }
         public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { CatigoryId = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { CatigoryId = 2, Name = "Scifi", DisplayOrder = 2},
+                new Category { CatigoryId = 3, Name = "History", DisplayOrder = 3},
+                new Category { CatigoryId = 4, Name = "DarkRomance", DisplayOrder = 4 }
+                );
+        }
     }
 }
